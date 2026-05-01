c7280120cdde813aece7e19474db0173fedbc8af
Rey <reyunity88@gmail.com>
2026-05-01 21:25:59 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const elements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=> {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+elements.forEach(el => observer.observe(el));
+const elements1 = document.querySelectorAll('.hidden1');
+
+const observer1 = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=> {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+elements1.forEach(el => observer1.observe(el));
